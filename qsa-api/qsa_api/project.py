b27a600dc4ef106aa713da442424bd4d241530bc
JeromeKsiki <j.ksigzkiewiez@promethee.earth>
2024-11-06 14:16:37 +0100

    log uri

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -514,6 +514,7 @@ class QSAProject:
 
         # init renderer
         tif = Path(__file__).resolve().parent / "raster" / "empty.tif"
+        self.debug("tif" + tif.as_uri)
         rl = QgsRasterLayer(tif.as_posix(), "", "gdal")
 
         # symbology
