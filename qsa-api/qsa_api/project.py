da3f17117fdd02889cce169121795a1b472e9b09
j rougier <j.rougier@promethee.earth>
2025-02-17 16:54:34 +0100

    test w constant

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -575,7 +575,7 @@ class QSAProject:
         self.__process_renderering(rl, rendering)
         self.debug(f"Rendering : start")
         rl.setContrastEnhancement(
-            QgsContrastEnhancement.ContrastEnhancementAlgorithm.UserDefinedEnhancement,
+            QgsContrastEnhancement.ContrastEnhancementAlgorithm.StretchToMinimumMaximum,
             QgsRasterMinMaxOrigin.Limits.CumulativeCut,
         )
         self.debug(f"Rendering : contrast enhancement set")
