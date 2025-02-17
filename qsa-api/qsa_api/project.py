7306a088810fd284d76d979555f349536b224fa1
j rougier <j.rougier@promethee.earth>
2025-02-17 16:33:57 +0100

    divide by 100

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -580,8 +580,8 @@ class QSAProject:
         )
         self.debug(f"Rendering : contrast enhancement set")
         
-        self.debug(f"max cumulative cut : 90")
-        self.debug(f"min cumulative cut : 10")
+        self.debug(f"max cumulative cut : .90")
+        self.debug(f"min cumulative cut : .10")
         renderer.setCumulativeCut(rl, (90.) / 100, (10.) / 100)
         self.debug(f"Rendering : cumulative cut set")
         rl.setRenderer(renderer.renderer)
