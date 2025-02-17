cf6390c3a9399f4b47de7ae93ea76ced1a4125ec
Jérôme Ksigzkiewiez <j.ksigzkiewiez@promethee.earth>
2025-02-17 20:13:55 +0100

    test7

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -584,7 +584,9 @@ class QSAProject:
         self.debug(f"min cumulative cut : .10")
         renderer.setCumulativeCut(rl, (90.) / 100, (10.) / 100)
         self.debug(f"Rendering : cumulative cut set")
-        rl.setRenderer(renderer.renderer)
+        
+        rl.renderer().renderer().cumulativeCut(1,10.,90.)
+        # rl.setRenderer(renderer.renderer)
         # self.debug(f"Rendering : renderer set")
         # rl.triggerRepaint()
         self.debug(f"Rendering : trigger repaint")
