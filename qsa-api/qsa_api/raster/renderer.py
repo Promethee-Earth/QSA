39a3e8add30d370cc1acec4c5f3b3e84c7715eb4
j rougier <j.rougier@promethee.earth>
2025-02-25 14:35:00 +0100

    add cumulative cut on gray band

--- qsa-api/qsa_api/raster/renderer.py
+++ qsa-api/qsa_api/raster/renderer.py
@@ -256,7 +256,7 @@ class RasterSymbologyRenderer:
         
 
     def _refresh_min_max_singlebandgray(self, layer: QgsRasterLayer) -> None:
-        renderer = QgsSingleBandGrayRenderer(layer.renderer())
+        renderer = QgsSingleBandGrayRenderer(layer.renderer(), 1)
         ce = renderer.contrastEnhancement()
 
         # early break
